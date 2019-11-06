6fe281be0169b53e46ce85ded1752562fe229041
Oscar <OscarGim4>
2019-11-06 22:44:25 +0100

    :tada: Commit Inicial

---
+++ src/org/japo/java/main/Main.java
@@ -0,0 +1,52 @@
+/* 
+ * Copyright 2019 Oscar G.4 - oscargimenez4@gmail.com.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.japo.java.main;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Oscar G.4 - oscargimenez4@gmail.com
+ */
+public class Main {
+
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+        //Variables
+        boolean parOK = false;
+        boolean testOK = false;
+        int n = 0;
+        do {
+            do {
+                try {
+                    System.out.print("Introduzca un numero par...: ");
+                    n = SCN.nextInt();
+                    testOK = true;
+                } catch (Exception e) {
+                    System.out.println("ERROR: Dato incorrecto");
+                } finally {
+                    SCN.nextLine();
+                }
+            } while (!testOK);
+            parOK = n % 2 == 0;
+        } while (!parOK);
+        System.out.printf("Su numero par es...: %d%n", n);
+    }
+}
